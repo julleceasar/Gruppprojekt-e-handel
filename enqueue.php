8c3dafb4f95de88a0a42110a12161219662fffa0
Julius Djudjaku <juliusdjudjaku@gmail.com>
2022-04-14 11:56:18 +0200

    header and woocommerce files

--- enqueue.php
+++ enqueue.php
@@ -6,13 +6,19 @@
 function load_scripts() {
 
     // CSS
-    wp_enqueue_style("bootstrap", get_template_directory_uri()."/css/bootstrap.css");
+    wp_enqueue_style("bootstrap", get_template_directory_uri()."/asset/css/bootstrap.min.css");
     wp_enqueue_style("font-awesome", get_template_directory_uri()."/css/font-awesome.css");
-    wp_enqueue_style("style", get_template_directory_uri()."/css/style.css");
-
+    wp_enqueue_style("style", get_template_directory_uri()."/style.css");
+    
     // JS
+    wp_enqueue_script("bootstrap", get_template_directory_uri()."/asset/js/bootstrap.min.js");
+    wp_enqueue_script("bootstrap", get_template_directory_uri()."/asset/js/bootstrap.js");
     wp_enqueue_script('jquery', get_template_directory_uri().'/js/jquery.js'); 
     wp_enqueue_script('script', get_template_directory_uri().'/js/script.js'); 
+    wp_enqueue_style('bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css');
+    wp_enqueue_script( 'boot1','https://code.jquery.com/jquery-3.3.1.slim.min.js', array( 'jquery' ),'',true );
+    wp_enqueue_script( 'boot2','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array( 'jquery' ),'',true );
+    wp_enqueue_script( 'boot3','https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array( 'jquery' ),'',true );
 
 }
 
--- header.php
+++ header.php
@@ -1,35 +1,75 @@
 <!DOCTYPE html>
 <html>
 
-<head> 
-	<meta charset="UTF-8"/>
-    <title><?php wp_title(); ?></title>
-    <?php wp_head(); ?>
+<head>
+	<meta charset="UTF-8" />
+	<title><?php wp_title(); ?></title>
+	<?php wp_head(); ?>
 </head>
-  
-<body> 
 
+<body>
 
-	<div id="wrap">
 
 	<header id="header">
-			<div class="container">
-				<div class="row">
-					<div class="">
 
-                    <nav id="nav">
-				        <div class="container">
-					        <div class="row">
-						        <div class="">
+	<div class="banner">
+		<p class="banner-text">FÅ 30 % RABATT MED KODEN SHOP30</p>
+		<p class="banner-text">60 DAGARS* GRATIS BYTE OCH RETUR</p>
+		<p class="banner-text">Fri frakt och retur</p>
+	</div>
+		
 
-                            <!-- Navigerings-Meny -->
-							<?php  wp_nav_menu(array(
-							   'theme_location' => 
-							   ''
-							   ));
-						   ?>
-						</div>
-					</div>
-				</div>
-			</nav>
-		</header>
+    </div>
+	
+    
+  </div>
+
+		<nav class="navbar navbar-expand-sm navbar-light bg-light">
+			<a href="#" class="navbar-brand">SportCeasar</a>
+			<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarMenu">
+				<ul class="navbar-nav m-auto">
+				
+					<!-- <li class="nav-item">
+						<a href="#" class="nav-link">BAJS</a>
+					 </li>  -->
+					 
+					 <li class="nav-item">
+						<!-- <a href="#" class="nav-link">KISS</a>  -->
+						<?php $menuarray = [
+								   'theme_location' => 'headermenu',
+								   /* 'menu' => ' ',
+								   'container' => 'div',
+								   'menu_class' => 'collapse navbar-collapse',
+								   'container_class' => 'collapse navbar-collapse' */
+								   
+								   ]; ?>
+			  
+								  <?php wp_nav_menu($menuarray); ?>	
+						
+						
+					 </li> 
+				 
+
+	
+				</ul> 
+				
+				<!-- <ul class="navbar-nav">
+					<li class="nav-item">
+						<a href="#" class="nav-link">Users</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link">Products</a>
+					</li> -->
+					
+
+					<?php get_search_form(); ?>
+					
+				</ul>
+			</div>
+		</nav>
+
+
+	</header>
