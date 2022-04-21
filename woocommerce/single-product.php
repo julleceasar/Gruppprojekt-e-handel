7b6fa616ea8a73093714c006cd2799d2c576cc74
Lisa Karnerfors <lisa.karnerfors@live.com>
2022-04-21 15:25:06 +0200

    Changed price

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 
 <!-- Product section-->
- <section class="py-5">
+        <section class="py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="row gx-4 gx-lg-5 align-items-center">
                     <div class="col-md-6">
@@ -10,21 +10,19 @@
 							while (have_posts()) { 
 								the_post();?>
                         
-                        <img class="card-img-top mb-5 mb-md-0"> <?php the_post_thumbnail();?> </div>
-                    
-                        <div class="col-md-6">
-                            <div class="small mb-1"><!-- Artikelnr --></div> 
-
-                            <h1 class="display-5 fw-bolder"><?php the_title();?></h1>
-                        
-                           
+                            <img class="card-img-top mb-5 mb-md-0"> <?php the_post_thumbnail();?> </div>
+                                
+                            <div class="col-md-6">
+                                <div class="small mb-1"><!-- Artikelnr -->
+                            </div> 
+                                <h1 class="display-5 fw-bolder"><?php the_title();?></h1>
                             <div class="fs-5 mb-5"> 
-                                <span class="text-decoration-line-through"><!-- Get price --></span>
+                                <h5><?php echo $product->get_price();?></h5>
                             </div>
 
                             <p class="lead"><?php the_content();?></p>
 
-                            <!-- Ta bort? -->
+                            <!-- Ta bort? & ersätt -->
                             <div class="d-flex">
                                 <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
                             <button class="btn btn-outline-dark flex-shrink-0" type="button">
