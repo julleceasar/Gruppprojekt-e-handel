f54d07ff1024c6ce51f43da0fb615c39c8efb56d
Julius Djudjaku <juliusdjudjaku@gmail.com>
2022-05-02 15:22:23 +0200

    butiksidor med mera

--- archive-butikinfo.php
+++ archive-butikinfo.php
@@ -8,7 +8,7 @@
         the_post();
         ?>
 
-<div id="content-wrap" class="container clr">
+        <div id="content-wrap" class="container clr">
             <?php the_content(); ?>
             <a href="<?php the_permalink(); ?> "><h1><?php the_title(); ?></h1></a>
              
